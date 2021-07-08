0485b314262905297e9b3d62670d88a9c0b4bf0f
PORTES <fabien.portes@sogeti.com>
2021-07-08 08:42:34 +0200

    fix glue job writing mode from append to overwrite

--- services/create_update_job/template/transform_sql_query.py
+++ services/create_update_job/template/transform_sql_query.py
@@ -23,14 +23,16 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+# update hadoop conf to avoid creation of empty folders $_folder_$
+hadoop_conf = sc._jsc.hadoopConfiguration()
+hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
 {DATASOURCES}
 
 Transform0 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {DATASOURCE_MAPPING}, transformation_ctx = "Transform0")
 
-DataSink0 = glueContext.getSink(path = "s3://meteostacklabsglued/{S3_FOLDER}/", connection_type = "s3", updateBehavior = "UPDATE_IN_DATABASE", partitionKeys = [], compression = "snappy", enableUpdateCatalog = True, transformation_ctx = "DataSink0")
-DataSink0.setCatalogInfo(catalogDatabase = "meteo_s3", catalogTableName = "{TABLE_NAME}")
-DataSink0.setFormat("glueparquet")
-DataSink0.writeFrame(Transform0)
-
+Transform0 = Transform0.toDF()
+# write data with spark write method as dynamic frame context doesn't support overwrite
+Transform0.write.mode('overwrite').parquet('s3://meteostacklabsglued/{S3_FOLDER}')
 
 job.commit()'''
